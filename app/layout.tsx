f76ec3dfe1b8dc80e83a8270e41e15b04018eb85
javayhu <javayhu@gmail.com>
2024-12-23 01:21:09 +0800

    chore: add plausible analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,11 +15,13 @@ import { Toaster } from "@/components/ui/sonner";
 import { ModeToggle } from "@/components/mode-toggle";
 import { MobileNav } from "@/components/nav/mobile-nav";
 import { ThemeProvider } from "@/components/theme-provider";
+import PlausibleProvider from "next-plausible";
 
 export const metadata: Metadata = {
 	metadataBase: new URL("https://og.indiehub.best"),
 	title: "Free OG Generator",
-	description: "Generate beautiful Open Graph images with zero effort, powered by IndieHub, the best all-in-one directory for indie makers.",
+	description:
+		"Generate beautiful Open Graph images with zero effort, powered by IndieHub, the best all-in-one directory for indie makers.",
 	openGraph: {
 		title: "indiehub",
 		description: "Generate beautiful Open Graph images with zero effort.",
@@ -50,41 +52,53 @@ export default async function RootLayout({
 			suppressHydrationWarning
 		>
 			<body className="min-h-screen bg-background font-sans antialiased">
-				<ThemeProvider
-					attribute="class"
-					defaultTheme="system"
-					enableSystem
-					disableTransitionOnChange
+				<PlausibleProvider
+					domain="og.indiehub.best"
+					// https://github.com/4lejandrito/next-plausible/blob/master/test/page/pages/customDomain.js
+					customDomain="https://pa.indieapp.site"
+					selfHosted={true}
+					trackOutboundLinks={true}
+					trackLocalhost={false}
+					taggedEvents={true}
+					hash={true}
+					// https://github.com/4lejandrito/next-plausible/blob/master/test/page/pages/revenue.js
+					revenue={false}
 				>
-					<nav className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4 sm:px-6 lg:px-8">
-						<div className="flex items-center space-x-4">
-							<div>
-								<Link href="/">
-									<Image
-										className="block dark:hidden"
-										src="/logo.png"
-										alt="Free OG Generator Logo"
-										width={36}
-										height={36}
-									/>
-								</Link>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="system"
+						enableSystem
+						disableTransitionOnChange
+					>
+						<nav className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4 sm:px-6 lg:px-8">
+							<div className="flex items-center space-x-4">
+								<div>
+									<Link href="/">
+										<Image
+											className="block dark:hidden"
+											src="/logo.png"
+											alt="Free OG Generator Logo"
+											width={36}
+											height={36}
+										/>
+									</Link>
 
-								<Link href="/">
-									<Image
-										className="hidden dark:block"
-										src="/logo.png"
-										alt="Free OG Generator Logo"
-										width={36}
-										height={36}
-									/>
-								</Link>
+									<Link href="/">
+										<Image
+											className="hidden dark:block"
+											src="/logo.png"
+											alt="Free OG Generator Logo"
+											width={36}
+											height={36}
+										/>
+									</Link>
+								</div>
+
+								<span className="text font-semibold">Free OG Generator</span>
 							</div>
 
-							<span className="text font-semibold">Free OG Generator</span>
-						</div>
-
-						<div className="hidden space-x-2 sm:flex">
-							{/* <Button variant="link">
+							<div className="hidden space-x-2 sm:flex">
+								{/* <Button variant="link">
 								<Link href="https://indiehub.best" target="_blank">
 									IndieHub
 								</Link>
@@ -96,72 +110,74 @@ export default async function RootLayout({
 								</Link>
 							</Button> */}
 
-							<ModeToggle />
-						</div>
+								<ModeToggle />
+							</div>
 
-						{/* Mobile navigation */}
-						<div className="flex space-x-2 sm:hidden">
-							<ModeToggle />
+							{/* Mobile navigation */}
+							<div className="flex space-x-2 sm:hidden">
+								<ModeToggle />
 
-							<MobileNav />
-						</div>
-					</nav>
+								<MobileNav />
+							</div>
+						</nav>
 
-					<main className="mx-auto min-h-[calc(100dvh-84px)] max-w-7xl px-2 py-6 sm:px-6 lg:px-8">
-						<TemplateStoreProvider>{children}</TemplateStoreProvider>
-					</main>
+						<main className="mx-auto min-h-[calc(100dvh-84px)] max-w-7xl px-2 py-6 sm:px-6 lg:px-8">
+							<TemplateStoreProvider>{children}</TemplateStoreProvider>
+						</main>
 
-					<Separator />
+						<Separator />
 
-					<footer className="mx-auto max-w-7xl px-2 py-6 sm:px-6 lg:px-8">
-						<div className="flex justify-between">
-							<div className="flex items-center space-x-2">
-								<div className="font-mono font-semibold">
-									<Link href="https://og.indiehub.best" target="_blank">
-										Free OG Generator
-									</Link>
-								</div>
+						<footer className="mx-auto max-w-7xl px-2 py-6 sm:px-6 lg:px-8">
+							<div className="flex justify-between">
+								<div className="flex items-center space-x-2">
+									<div className="font-mono font-semibold">
+										<Link href="https://og.indiehub.best" target="_blank">
+											Free OG Generator
+										</Link>
+									</div>
 
-								<div>
-									{/* <Button variant="link" asChild>
+									<div>
+										{/* <Button variant="link" asChild>
                     <Link href="/">Home</Link>
                   </Button> */}
 
-									<Button variant="link" asChild>
-										<Link href="https://indiehub.best" target="_blank">
-											IndieHub - The best all-in-one directory for indie makers
-										</Link>
-									</Button>
+										<Button variant="link" asChild>
+											<Link href="https://indiehub.best" target="_blank">
+												IndieHub - The best all-in-one directory for indie
+												makers
+											</Link>
+										</Button>
 
-									<Button variant="link" asChild>
-										<Link href="https://mkdirs.com" target="_blank">
-											Mkdirs - The best directory boilerplate
-										</Link>
-									</Button>
+										<Button variant="link" asChild>
+											<Link href="https://mkdirs.com" target="_blank">
+												Mkdirs - The best directory boilerplate
+											</Link>
+										</Button>
+									</div>
+								</div>
+
+								{/* only shows on Desktop */}
+								<div className="hidden items-center gap-x-2 md:inline-flex">
+									<svg
+										className="h-2 w-2 fill-green-400"
+										viewBox="0 0 6 6"
+										aria-hidden="true"
+									>
+										<circle cx={3} cy={3} r={3} />
+									</svg>
+									<span className="font-mono text-sm font-medium">
+										Operational
+									</span>
 								</div>
 							</div>
+						</footer>
+					</ThemeProvider>
 
-							{/* only shows on Desktop */}
-							<div className="hidden items-center gap-x-2 md:inline-flex">
-								<svg
-									className="h-2 w-2 fill-green-400"
-									viewBox="0 0 6 6"
-									aria-hidden="true"
-								>
-									<circle cx={3} cy={3} r={3} />
-								</svg>
-								<span className="font-mono text-sm font-medium">
-									Operational
-								</span>
-							</div>
-						</div>
-					</footer>
-				</ThemeProvider>
+					<Toaster />
 
-				<Toaster />
-
-				<SpeedInsights />
-				<Analytics />
+					<SpeedInsights />
+					<Analytics />
+				</PlausibleProvider>
 			</body>
 		</html>
 	);
